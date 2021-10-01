837afe76c9fd520d7db3b059ef8a9ba22a80ca0d
arthur kim <znxkznxk1030@gmail.com>
2021-10-01 23:10:45 +0900

    Day - 6 | styling

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -8,13 +8,11 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={(event) => this.setState({ term: event.target.value })}
         />
-        <br />
-        Value of the input: {this.state.term}
       </div>
     );
   }
